16e7512007493bf025c188ef0a271ae42633e974
Marc <jo@marcllucia.com>
2011-04-04 20:42:08 +0200

    cartes centrades al mig de la taula

--- tableCard.cpp
+++ tableCard.cpp
@@ -30,7 +30,11 @@ TableCard::TableCard(const TableCard&)
 void TableCard::draw()
 {
     ofPushMatrix();
-    ofTranslate(0.5,0.5,0);
+    ofTranslate(-0.0247487373+0.5-0.120208153,-0.0247487373+0.5-0.120208153,0);
+    //ofTranslate(0,0.049,0);
+    ofRotateZ(45);
+    ofTranslate(0,-0.049,0);
+    //ofTranslate((area.x+area.height/2),(area.y+area.width/2),0);
     //ofTranslate(-(area.x+area.height/2),-(area.y+area.width/2),0);
     texture.draw(area.x,area.y,area.height,area.width);
     ofPopMatrix();
